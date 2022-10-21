cf7b12c588dddcf258b6f35766ad7b35b735fa7d
Joseph Hill <joseph.hill1@outlook.com>
2022-10-21 22:14:13 +0000

    const and let

---
+++ ch02/example.js
@@ -0,0 +1,10 @@
+"use strict";
+/* const & let */
+let index = 0;
+if (index == 0) {
+    let index = 2;
+    console.log(`index = ${index}`);
+}
+console.log(`index = ${index}`);
+const constValue = "this should not be changed";
+//# sourceMappingURL=example.js.map
---
+++ ch02/example.ts
@@ -0,0 +1,9 @@
+/* const & let */
+let index: number = 0;
+if (index == 0) {
+    let index: number = 2;
+    console.log(`index = ${index}`);
+}
+console.log(`index = ${index}`);
+
+const constValue = "this should not be changed";
