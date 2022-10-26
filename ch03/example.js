2694f56153f160d1bca2825a7d30bd27b651a8b9
Joseph Hill <joseph.hill1@outlook.com>
2022-10-26 01:42:44 +0000

    added deno support

--- ch03/example.js
+++ ch03/example.js
@@ -1,4 +1,16 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _ClassES6Private_id;
 const user = {
     id: 1,
     name: "Joseph",
@@ -34,5 +46,92 @@ function getProperty(key, value) {
 }
 getProperty("id", { id: 1, name: "firstName" });
 getProperty("name", { id: 2, name: "secondName" });
-getProperty("telephone", { id: 3, name: "thirdName" });
+//getProperty("telephone", { id: 3, name: "thirdName" });
+/* CLASSES */
+class SimpleClass {
+    print() {
+        console.log(`SimpleClass.print() called.`);
+    }
+}
+let mySimpleClass = new SimpleClass();
+mySimpleClass.print();
+/* 'THIS' KEYWORD */
+class SimpleClass2 {
+    print() {
+        console.log(`SimpleClass2.id = ${this.id}`);
+    }
+}
+let mySimpleClass2 = new SimpleClass2();
+mySimpleClass2.id = 2020;
+mySimpleClass2.print();
+/* IMPLEMENTING INTERFACES */
+class ClassA {
+    print() {
+        console.log(`ClassA.print() called.`);
+    }
+}
+class ClassB {
+    print() {
+        console.log(`ClassB.print() called.`);
+    }
+}
+function printClass(a) {
+    a.print();
+}
+let classA = new ClassA();
+let classB = new ClassB();
+printClass(classA);
+printClass(classB);
+class ClassC {
+    print() {
+        console.log(`ClassC.print() called.`);
+    }
+}
+let classC = new ClassC();
+printClass(classC);
+/* CLASS CONSTRUCTORS */
+class ClassWithConstructor {
+    constructor(id) {
+        this.id = id;
+    }
+}
+let classWithConstructor = new ClassWithConstructor(10);
+console.log(`classWithConstructor = ${JSON.stringify(classWithConstructor)}`);
+/* CLASS MODIFIERS */
+class ClassWithPublicProperty {
+}
+let publicAccess = new ClassWithPublicProperty();
+publicAccess.id = 10;
+class ClassWithPrivateProperty {
+    constructor(id) {
+        this.id = id;
+    }
+}
+let privateAccess = new ClassWithPrivateProperty(20);
+console.log(JSON.stringify(publicAccess));
+console.log(JSON.stringify(privateAccess));
+/* JS PRIVATE FIELDS */
+class ClassES6Private {
+    constructor(id) {
+        _ClassES6Private_id.set(this, void 0);
+        __classPrivateFieldSet(this, _ClassES6Private_id, id, "f");
+    }
+    print() {
+        console.log(`es6 private class = ${__classPrivateFieldGet(this, _ClassES6Private_id, "f")}`);
+    }
+}
+_ClassES6Private_id = new WeakMap();
+let es6PrivateClass = new ClassES6Private(10);
+console.log(`es6 private class = ${JSON.stringify(es6PrivateClass)}`);
+es6PrivateClass.print();
+/* CONSTRUCTOR PARAMETER PROPERTIES */
+class ClassWithCtorMods {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+let myClassMod = new ClassWithCtorMods(1, "test");
+console.log(`myClassMod.id = ${myClassMod.id}`);
+console.log(`myClassMod.name = ${myClassMod.name}`);
 //# sourceMappingURL=example.js.map
--- ch03/example.ts
+++ ch03/example.ts
@@ -68,4 +68,108 @@ function getProperty(key: PersonPropertyName, value: Person) {
 }
 getProperty("id", { id: 1, name: "firstName" });
 getProperty("name", { id: 2, name: "secondName" });
-getProperty("telephone", { id: 3, name: "thirdName" });
+//getProperty("telephone", { id: 3, name: "thirdName" });
+
+/* CLASSES */
+class SimpleClass {
+  id?: number;
+  print(): void {
+    console.log(`SimpleClass.print() called.`);
+  }
+}
+let mySimpleClass = new SimpleClass();
+mySimpleClass.print();
+
+/* 'THIS' KEYWORD */
+class SimpleClass2 {
+  id?: number;
+  print(): void {
+    console.log(`SimpleClass2.id = ${this.id}`);
+  }
+}
+let mySimpleClass2 = new SimpleClass2();
+mySimpleClass2.id = 2020;
+mySimpleClass2.print();
+
+/* IMPLEMENTING INTERFACES */
+class ClassA implements Print {
+  print(): void {
+    console.log(`ClassA.print() called.`);
+  }
+}
+class ClassB implements Print {
+  print(): void {
+    console.log(`ClassB.print() called.`);
+  }
+}
+
+interface Print {
+  print(): void;
+}
+function printClass(a: Print) {
+  a.print();
+}
+
+let classA = new ClassA();
+let classB = new ClassB();
+printClass(classA);
+printClass(classB);
+
+class ClassC {
+  print(): void {
+    console.log(`ClassC.print() called.`);
+  }
+}
+let classC = new ClassC();
+printClass(classC);
+
+/* CLASS CONSTRUCTORS */
+class ClassWithConstructor {
+  id: number;
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+
+let classWithConstructor = new ClassWithConstructor(10);
+console.log(`classWithConstructor = ${JSON.stringify(classWithConstructor)}`);
+
+/* CLASS MODIFIERS */
+class ClassWithPublicProperty {
+  public id?: number;
+}
+let publicAccess = new ClassWithPublicProperty();
+publicAccess.id = 10;
+
+class ClassWithPrivateProperty {
+  private id: number;
+  constructor(id: number) {
+    this.id = id;
+  }
+}
+let privateAccess = new ClassWithPrivateProperty(20);
+
+console.log(JSON.stringify(publicAccess));
+console.log(JSON.stringify(privateAccess));
+
+/* JS PRIVATE FIELDS */
+class ClassES6Private {
+  #id: number;
+  constructor(id: number) {
+    this.#id = id;
+  }
+  print() {
+    console.log(`es6 private class = ${this.#id}`);
+  }
+}
+let es6PrivateClass = new ClassES6Private(10);
+console.log(`es6 private class = ${JSON.stringify(es6PrivateClass)}`);
+es6PrivateClass.print();
+
+/* CONSTRUCTOR PARAMETER PROPERTIES */
+class ClassWithCtorMods {
+  constructor(public id: number, private name: string) {}
+}
+let myClassMod = new ClassWithCtorMods(1, "test");
+console.log(`myClassMod.id = ${myClassMod.id}`);
+console.log(`myClassMod.name = ${myClassMod.name}`);
