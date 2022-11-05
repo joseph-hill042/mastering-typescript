6be506fe5d1e91a1f1021182a38a632b5c356c38
Joseph Hill <joseph.hill1@outlook.com>
2022-11-05 00:03:30 +0000

    modules--import/export, renaming, multiple exports

---
+++ ch03/modules-main.ts
@@ -0,0 +1,19 @@
+/* -----MODULES----- */
+
+/* IMPORTING & EXPORTING MODULES */
+import { Module1 } from "./modules/Module1.ts";
+const mod1 = new Module1();
+mod1.print();
+
+/* MODULE RENAMING */
+import { Module1 as MyMod1 } from "./modules/Module1.ts";
+const myRenamedMod = new MyMod1();
+myRenamedMod.print();
+
+/* MULTIPLE EXPORTS */
+import { MultipleClass1, MultipleClass2 } from "./modules/MultipleExports.ts";
+const mc1 = new MultipleClass1();
+const mc2 = new MultipleClass2();
+
+mc1.print();
+mc2.print();
---
+++ ch03/modules/MultipleExports.ts
@@ -0,0 +1,10 @@
+export class MultipleClass1 {
+  print(): void {
+    console.log(`MultipleClass1.print() called`);
+  }
+}
+export class MultipleClass2 {
+  print(): void {
+    console.log(`MultipleClass2.print() called`);
+  }
+}
